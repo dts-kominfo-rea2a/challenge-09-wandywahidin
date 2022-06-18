95839fdfc2ff20c01ba7c50660f88335b0b4769d
wandy <wandywahidin53@gmail.com>
2022-06-18 15:03:31 +0700

    Tugas 9

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import './App.css';
+import "./App.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import contacts from "./data/contacts.json";
 
 // Uncomment untuk memuat daftar kontak
 // import contacts from './data/contacts.json';
@@ -7,9 +10,18 @@ const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header />
+      <ul>
+        {contacts.map((item) => {
+          return (
+            <li key={item.phone}>
+              <Contact data={item} />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,19 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import '../components/Contact.css'
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  return (
+    <div className="contact">
+      <img src={data.photo} alt="gambar profil" />
+      <div className="item">
+        <h2>{data.name}</h2>
+        <p>{data.phone}</p>
+        <p>{data.email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
